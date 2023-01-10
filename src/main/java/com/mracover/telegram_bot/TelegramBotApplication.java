ab9e29c8fafa0ace72344092d2c11ac1296241d6
mracover <mracover@gmail.com>
2023-01-11 02:00:20 +0500

    init commit (+ docker for mongoDB)

---
+++ src/main/java/com/mracover/telegram_bot/TelegramBotApplication.java
@@ -0,0 +1,16 @@
+package com.mracover.telegram_bot;
+
+import com.mracover.telegram_bot.controller.TelegramBot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
+
+}
