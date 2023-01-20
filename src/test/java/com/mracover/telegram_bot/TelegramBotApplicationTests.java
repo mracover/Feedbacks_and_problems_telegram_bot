68a5982a1ddb5452b4f7d9fa8ee402fb59513a2d
mracover <mracover@gmail.com>
2023-01-20 20:08:14 +0500

    Dockerfile

--- src/test/java/com/mracover/telegram_bot/TelegramBotApplicationTests.java
+++ src/test/java/com/mracover/telegram_bot/TelegramBotApplicationTests.java
@@ -1,14 +1,8 @@
 package com.mracover.telegram_bot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TelegramBotApplicationTests {
-
-    @Test
-    void contextLoads() {
-
-    }
 
 }
